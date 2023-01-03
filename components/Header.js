6c65cb4f19afad7bfff15e8966c9bac1313f22b0
codeyStein <san.26.2010@icloud.com>
2023-01-03 12:10:35 -0600

    Add Underline to Header Component

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,14 @@ import React from 'react'
 import style from '../styles/Header.module.css'
 
 export default function Header(props) {
-    return(
-        <h1 className={style.header}>{props.label}</h1>
+
+    const isPrimary = props.primary
+
+    return (
+        <>
+            {isPrimary && <h1 className={`${style.header} ${style.primary}`}>{props.label}</h1>}
+            {isPrimary==false && <h2 className={style.header}>{props.label}</h2>}
+        </>
     )
 }
 
