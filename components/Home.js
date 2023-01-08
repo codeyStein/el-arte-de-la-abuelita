2fcd11d4268b2aa1fcfa78c7e3ae12986c72f370
codeyStein <san.26.2010@icloud.com>
2023-01-07 19:43:20 -0600

    Add Landing Section in Home

---
+++ components/Home.js
@@ -0,0 +1,19 @@
+import Header from './Header.js'
+import Text from './Text.js'
+import Button from './Button.js'
+import Link from 'next/link'
+
+export default function Home() {
+
+    return (
+        <header>
+            <Header label="El Arte de la Abuelita" primary={true} />
+            <Text style='mini' label="Bisuteria y manualidades a mano con madera"/>
+            <div>
+                <Button style='fill' label="Tienda" />
+                <Button style='outline' label="Instagram" />
+            </div>
+        </header>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 import Navbar from '../components/Navbar'
+import Home from '../components/Home.js'
 
-export default function Home() {
+export default function Index() {
   return (
     <>
+      <NextSeo
+        title="El Arte de la Abuelita"
+        titleTemplate='Inicio | %s'
+        description="Soy una señora de 79 años hago bisuteria, y trabajo con madera a mano en El Salvador."
+        canonical="https://el-arte-de-la-abuelita.netlify.app"
+      />
       <Head>
-        <title>El Arte de la Abuelita</title>
-        <meta name="description" content="Soy una señora de 79 años hago bisuteria, y trabajo con madera a mano." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <Navbar />
-        <h1>Hello World</h1>
+        <Home />
       </main>
     </>
   )
