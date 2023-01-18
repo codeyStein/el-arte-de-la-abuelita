4a3b70679a747c468c736230826e0b2d1790d655
codeyStein <san.26.2010@icloud.com>
2023-01-17 18:46:02 -0600

    Open Graph SEO Optimization

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,17 @@ export default function Index() {
         titleTemplate='Inicio | %s'
         description="Soy una señora de 79 años hago bisuteria, y trabajo con madera a mano en El Salvador."
         canonical="https://el-arte-de-la-abuelita.netlify.app"
+        openGrap={{
+            url: "https://el-arte-de-la-abuelita.netlify.app",
+            title: 'El Arte de la Abuelita',
+            description: "Soy una abuelita de 79 años hago trabajo apasionado con bisuteria, y trabajo con madera a mano en El Salvador.",
+            images: [{
+                url: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-19/73387363_533718470744631_1296071016530837504_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=0118Y-RM6tsAX9g4740&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDKXaB6f2FM2lYCi8libw6q8J0bSHmln0SAk-VqEpVvag&oe=63CC26CE&_nc_sid=8fd12b",
+                width: 150,
+                height: 150,
+                alt: "Soy una abuelita de 79 años hago trabajo apasionado con bisuteria, y trabajo con madera a mano en El Salvador"
+            }]
+        }}
       />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
