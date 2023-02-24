c713ad9c5df64d6efb045ab9a78e63534d2f58bc
codeyStein <san.26.2010@icloud.com>
2023-02-24 11:09:13 -0600

    FIX: Section Card Images not Loading

--- data/sections.js
+++ data/sections.js
@@ -3,18 +3,21 @@ export default [
     id: "1",
     label: "Masetas",
     alt: "Photo Tienda Masetas",
-    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/323817523_875258733510450_466668763719734847_n.jpg?stp=cp6_dst-jpg_e35_fr&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=7HuBKen7vdoAX_d4j2-&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfB3tMZ8PXLOsdP4Bqwom4mWP9MMWZetI-N23czth6sc_A&oe=63DE18D7&_nc_sid=4f375e"
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/318667043_676833953842398_1417912711646146439_n.jpg?stp=dst-jpg_e35_s1080x1080&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=sDnooyhEp00AX9Y6nit&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfDpsGe9CGTkgjSAvDE9L3MGpUPYL_e0U7O_JAswaBld2A&oe=63EFD31F&_nc_sid=4f375e",
+    href: "/tienda/masetas"
   },
   {
     id: "2",
     label: "Bisuteria",
     alt: "Photo Tienda Bisuteria",
-    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/319007272_953034392535239_5463486077603625962_n.jpg?stp=cp6_dst-jpg_e35_fr&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=e3KBfNJN494AX8quOfA&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfAZ4-e7VONQCADV88OaQ0ai7wfBEu3g6UqgYD8-hYXGjw&oe=63DF29BF&_nc_sid=4f375e"
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/319007272_953034392535239_5463486077603625962_n.jpg?stp=cp6_dst-jpg_e35_fr&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=DXQTL5kJefQAX_P_CE3&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfCYBGI98pzWkA7_e1LCi8Voe_Iv3yxh8IndVpPsXg-D8Q&oe=63F8DEFF&_nc_sid=8fd12b",
+    href: "/tienda/bisuteria"
   },
   {
     id: "3",
     label: "Madera",
     alt: "Photo Tienda Madera",
-    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271985140_995161601209909_5377091902237457592_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=b3YiF6CW0_oAX-3PCXo&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfAy71iF_nePvWlAUGfaO0SXh2ULy68AWuxGS1l3v3GOfw&oe=63DE0433&_nc_sid=4f375e"
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271990231_612051753188090_146358143268347992_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=hSWHoqY3rAUAX8lkYk0&edm=APU89FABAAAA&ccb=7-5&oh=00_AfAkSiN77tFOypSdLxaRk6wMJ5y1YPEpcpZvhJYsBEflMQ&oe=63FE8F1F&_nc_sid=86f79a",
+    href: "/tienda/madera"
   }
 ]
