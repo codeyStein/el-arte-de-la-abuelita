f884b4830cafbb5813c783457cc2b4ebbe0aa82b
codeyStein <san.26.2010@icloud.com>
2023-04-04 10:50:14 -0600

    FEAT: Add Madera Page Data

---
+++ data/products/madera.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: "Casita De Madera",
+    price: "26.00",
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271990231_612051753188090_146358143268347992_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=chpVq-C3FK8AX94kZhp&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfCN-0kid4K5GPM_3EhuYXnL3uL-p-hDakl0Akfos6RurA&oe=6431F99F&_nc_sid=4f375e",
+    alt: "Photo Tienda Masetas",
+  },
+  {
+    name: "Casita De Madera",
+    price: "26.00",
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271990231_612051753188090_146358143268347992_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=chpVq-C3FK8AX94kZhp&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfCN-0kid4K5GPM_3EhuYXnL3uL-p-hDakl0Akfos6RurA&oe=6431F99F&_nc_sid=4f375e",
+    alt: "Photo Tienda Masetas",
+  },
+  {
+    name: "Casita De Madera",
+    price: "26.00",
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271990231_612051753188090_146358143268347992_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=chpVq-C3FK8AX94kZhp&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfCN-0kid4K5GPM_3EhuYXnL3uL-p-hDakl0Akfos6RurA&oe=6431F99F&_nc_sid=4f375e",
+    alt: "Photo Tienda Masetas",
+  },
+  {
+    name: "Casita De Madera",
+    price: "26.00",
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271990231_612051753188090_146358143268347992_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=chpVq-C3FK8AX94kZhp&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfCN-0kid4K5GPM_3EhuYXnL3uL-p-hDakl0Akfos6RurA&oe=6431F99F&_nc_sid=4f375e",
+    alt: "Photo Tienda Masetas",
+  },
+  {
+    name: "Casita De Madera",
+    price: "26.00",
+    src: "https://instagram.fsal1-1.fna.fbcdn.net/v/t51.2885-15/271990231_612051753188090_146358143268347992_n.webp?stp=dst-jpg_e35&_nc_ht=instagram.fsal1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=chpVq-C3FK8AX94kZhp&edm=AP_V10EBAAAA&ccb=7-5&oh=00_AfCN-0kid4K5GPM_3EhuYXnL3uL-p-hDakl0Akfos6RurA&oe=6431F99F&_nc_sid=4f375e",
+    alt: "Photo Tienda Masetas",
+  },
+]
