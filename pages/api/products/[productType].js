b562d4e352afcdfc87c083208bba5b9059ba3ec3
codeyStein <san.26.2010@icloud.com>
2023-04-04 10:53:12 -0600

    FEAT: Add Madera Page to API Endpoint

--- pages/api/products/[productType].js
+++ pages/api/products/[productType].js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import masetaData from "../../../data/products/masetas"
 import bisuteriaData from "../../../data/products/bisuteria"
+import maderaData from "../../../data/products/madera"
 
 const masetas = masetaData.map(item => {
     return {
@@ -20,6 +21,15 @@ const bisuteria = bisuteriaData.map(item => {
     }
 })
 
+const madera = maderaData.map(item => {
+    return {
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        img: item.src
+    }
+})
+
 
 export default function handler(req, res) {
     const query = req.query;
@@ -31,10 +41,13 @@ export default function handler(req, res) {
         } // 'bisuteria' query
         else if (productType=='bisuteria') {
             res.status(200).json({bisuteria})
+        } // 'madera' query
+        else if (productType=='madera') {
+            res.status(200).json({madera})
         } // null query
         else if (productType.length==0) {
-            res.status(200).json({masetas, bisuteria})
+            res.status(200).json({masetas, bisuteria, madera})
         } // invalid query
-        else { res.status(400).send({ error: 'failed to fetch data; invalid endpoint', status: 400 }) }
+        else { res.status(400).send({ error: 'failed to fetch data : invalid endpoint', status: 400 }) }
 
 }
